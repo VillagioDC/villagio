d3d80e48a50fb6426231f68775217f198353938a
VillagioDC <villagio.dc@gmail.com>
2024-07-18 00:30:09 -0300

    Update

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,9 @@
 // Navigation functions
 
+function openFIIPage() {
+    window.open('https://ondafii.onrender.com', '_blank');
+}
+
 function openToolsPage() {
     window.open('./pages/tools.html', '_self');
 }
