3b6a069de7b1a564dfa6f820c74391c6a6de365d
VillagioDC <villagio.dc@gmail.com>
2025-04-10 23:11:06 -0300

    Initial commit

---
+++ scripts/home.js
@@ -0,0 +1,17 @@
+// Navigation functions
+
+function openFIIPage() {
+    window.open('https://ondafii.onrender.com', '_blank');
+}
+
+function openToolsPage() {
+    window.open('./pages/tools.html', '_self');
+}
+
+function openAIPage() {
+    window.open('https://aichat-38nm.onrender.com', '_self');
+}
+
+function openFlowerOfLifePage() {
+    window.open('https://www.youtube.com/channel/UCgdfWRjQmBPKM9SBtJ8sVLA', '_blank');
+}
